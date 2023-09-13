147d9ba37284c67736ddb7b35a4994df5bfe88ea
존 <94162057+rogJohn01@users.noreply.github.com>
2023-09-13 12:37:21 +0900

    feat[statistics] : add get-calendar_data api axios
    A personal milestone

--- src/pages/statistics.js
+++ src/pages/statistics.js
@@ -1,16 +1,48 @@
+import MyResponsiveCalendar from '../components/graphs/calendar';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
 
-import MyResponsiveCalendar from '../components/graphs/calendar'
-import calendarData from '../components/graphs/calendarData'
+
+const formatCalendarData = (apiData) => {
+    return apiData.map(item => {
+        const localDate = moment.utc(item.dates).local().format('YYYY-MM-DD');
+        return {
+            value: item.date_count,
+            day: localDate
+        };
+    });
+};
+
+
+const fetchCalendarData = async (setCalendarData) => {
+    try {
+        const response = await axios.get('http://localhost:3006/statistics/get_calendar');
+        const formattedData = formatCalendarData(response.data);
+        console.log("Formatted data:", formattedData); // Debug line
+        setCalendarData(formattedData);
+    } catch (error) {
+        console.error('Error fetching calendar data:', error);
+    }
+};
+
 const Statistics = () => {
+    const [calendarData, setCalendarData] = useState([]);
+
+    useEffect(() => {
+        fetchCalendarData(setCalendarData);
+    }, []);
+
+    console.log("Current calendarData state:", calendarData); // Debug line
+
     return (
         <div>
             <h1>My </h1>
             <div style={{ height: 1000, width: 800 }}>
                 <MyResponsiveCalendar data={calendarData} />
             </div>
-
         </div>
     );
 };
 
-export default Statistics ;
+export default Statistics;
