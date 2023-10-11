90b0b0822905c2cb6bc2ce31913263dec8064055
존 <94162057+rogJohn01@users.noreply.github.com>
2023-10-11 15:31:48 +0900

    feat[word_view] : connect word_result api and process of integration 1/3
    The api data has only word_id. The api should join with word_list table and fetch also the word_name\front
    
    also experiencing some CSS issues between the sidebar and the datagrid

--- src/components/word_view/wordViewDatagrid.js
+++ src/components/word_view/wordViewDatagrid.js
@@ -1,42 +1,32 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import Tooltip from '@mui/material/Tooltip';
 import "./wordViewTable.css"; // Import the CSS file
 
+const WordViewTable2 = ({ data }) => {
+    console.log("Received data:", data);  // Debugging log
 
-const generateSampleData = (numRows) => {
-    const sampleData = [];
-    for (let i = 1; i <= numRows; i++) {
-        const word_id = Math.floor(Math.random() * 50) + 1;
-        const isCorrect = Math.floor(Math.random() * 2);
-        sampleData.push({
-            id: i,
-            word_id,
-            isCorrect,
-        });
-    }
-    return sampleData;
-};
-
-const WordViewTable2 = ({ numRows = 50 }) => {
-    const rawData = generateSampleData(numRows);
-    const groupedData = rawData.reduce((acc, item) => {
+    const groupedData = data.reduce((acc, item) => {
         if (!acc[item.word_id]) {
             acc[item.word_id] = [];
         }
-        acc[item.word_id].push(item.isCorrect ? 'green' : 'red');
+        acc[item.word_id].push({ color: item.isCorrect ? 'green' : 'red', date: item.word_date });
         return acc;
     }, {});
 
     const rows = Object.keys(groupedData).map((wordId, index) => ({
         id: index,
-        word_id: wordId,
-        ...groupedData[wordId].reduce((acc, color, i) => {
-            acc[`dot${i + 1}`] = color;
+        word_id: wordId.toString(),  // Explicitly set as string
+        ...groupedData[wordId].reduce((acc, { color, date }, i) => {
+            acc[`dot${i + 1}`] = { color, date };
             return acc;
         }, {}),
     }));
 
+    console.log("Generated rows:", rows);  // Debugging log
+
     const maxDots = Math.max(...Object.values(groupedData).map(arr => arr.length));
+
     const columns = [
         { field: 'word_id', headerName: 'Word ID', width: 150 },
         ...Array.from({ length: maxDots }, (_, i) => ({
@@ -44,17 +34,22 @@ const WordViewTable2 = ({ numRows = 50 }) => {
             headerName: `Dot ${i + 1}`,
             width: 100,
             renderCell: (params) => (
-                <div style={{ color: params.value || 'gray', fontSize: '24px' }}>•</div>
+                <Tooltip title={params.value ? params.value.date : ''} arrow>
+                    <div style={{ color: params.value?.color || 'white', fontSize: '50px' }}>
+                        •
+                    </div>
+                </Tooltip>
             ),
         })),
     ];
 
+    console.log("Generated columns:", columns);  // Debugging log
+
     return (
-        <div style={{ height: 400, width: '100%', margin: '20px' }}>
+        <div style={{ height: '100vh', width: 'calc(100% - 300px)', marginLeft: '200px', margin: '20px' }}>
             <DataGrid rows={rows} columns={columns} pageSize={5} />
         </div>
     );
 };
-
 
 export default WordViewTable2;
--- src/pages/wordResultTable.js
+++ src/pages/wordResultTable.js
@@ -1,9 +1,24 @@
 import WordViewTable2 from "../components/word_view/wordViewDatagrid";
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function WordResultTable() {
 
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3006/wordview/get_word_view_table')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
     return (
-        <WordViewTable2/>
-    )
-}
+        <div>
+            <WordViewTable2 data={data} />
+        </div>
+    );
+};
