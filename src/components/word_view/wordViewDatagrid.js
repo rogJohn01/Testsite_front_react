e67e82acc5f0d42effb83d95202b2c100c81da48
존 <94162057+rogJohn01@users.noreply.github.com>
2023-10-12 14:46:23 +0900

    feat[word_view] : complete add word_result table with api

--- src/components/word_view/wordViewDatagrid.js
+++ src/components/word_view/wordViewDatagrid.js
@@ -8,16 +8,20 @@ const WordViewTable2 = ({ data }) => {
 
     const groupedData = data.reduce((acc, item) => {
         if (!acc[item.word_id]) {
-            acc[item.word_id] = [];
+            acc[item.word_id] = {
+                word_front: item.word_front,
+                data: []
+            };
         }
-        acc[item.word_id].push({ color: item.isCorrect ? 'green' : 'red', date: item.word_date });
+        acc[item.word_id].data.push({ color: item.isCorrect ? 'green' : 'red', date: item.word_date });
         return acc;
     }, {});
 
     const rows = Object.keys(groupedData).map((wordId, index) => ({
         id: index,
-        word_id: wordId.toString(),  // Explicitly set as string
-        ...groupedData[wordId].reduce((acc, { color, date }, i) => {
+        word_front: groupedData[wordId].word_front,
+        word_id: wordId.toString(),
+        ...groupedData[wordId].data.reduce((acc, { color, date }, i) => {
             acc[`dot${i + 1}`] = { color, date };
             return acc;
         }, {}),
@@ -25,9 +29,10 @@ const WordViewTable2 = ({ data }) => {
 
     console.log("Generated rows:", rows);  // Debugging log
 
-    const maxDots = Math.max(...Object.values(groupedData).map(arr => arr.length));
+    const maxDots = Math.max(...Object.values(groupedData).map(arr => arr.data.length));
 
     const columns = [
+        { field: 'word_front', headerName: 'Word', width: 150 }, // New column for word_front
         { field: 'word_id', headerName: 'Word ID', width: 150 },
         ...Array.from({ length: maxDots }, (_, i) => ({
             field: `dot${i + 1}`,
