31dbd72ca2bea368bbaf206760b9a1d8490fccad
존 <94162057+rogJohn01@users.noreply.github.com>
2024-02-10 22:51:36 +0900

    complete main css layouts :good job!

--- src/pages/CardDashBoard.js
+++ src/pages/CardDashBoard.js
@@ -1,9 +1,31 @@
 
 import './CardDashBoard.css';
 
-
+import notebook_icon from '../../src/assets/images/notebook_icon.png'
 
 const CreateCardDashBoard = () => {
+
+    const folders = [
+        { name: 'Folder 1', icon: notebook_icon },
+        { name: 'Folder 2', icon: notebook_icon } ,
+        { name: 'Folder 1', icon: notebook_icon },
+        { name: 'Folder 2', icon: notebook_icon } ,
+        { name: 'Folder 1', icon: notebook_icon },
+        { name: 'Folder 2', icon: notebook_icon } ,
+        { name: 'Folder 1', icon: notebook_icon },
+        { name: 'Folder 2', icon: notebook_icon } ,
+        { name: 'Folder 2', icon: notebook_icon } ,
+        { name: 'add+', icon: notebook_icon } ,
+    ];
+
+    const FolderBox = ({ name, icon }) => {
+        return (
+            <div className="folder-box">
+                <img src={icon} alt={name} />
+                <p>{name}</p>
+            </div>
+        );
+    };
 
 
 
@@ -13,7 +35,9 @@ const CreateCardDashBoard = () => {
                 Hello
             </div>
             <div className="dash-body">
-
+                {folders.map((folder, index) => (
+                    <FolderBox key={index} name={folder.name} icon={folder.icon} />
+                ))}
             </div>
         </div>
     )
