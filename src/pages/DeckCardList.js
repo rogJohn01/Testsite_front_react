be180b8cc2e5be377fda86cc38823d3de0f6898e
존 <94162057+rogJohn01@users.noreply.github.com>
2024-02-12 16:34:40 +0900

    add deck-cardlist : complete main feature

--- src/pages/DeckCardList.js
+++ src/pages/DeckCardList.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import API from "../services/axiosConfig";
+import { useParams } from 'react-router-dom';
+
 
 const columns = [
     { field: 'word_id', headerName: 'ID', width: 90, type: 'number' },
     { field: 'word_front', headerName: 'Front', width: 150 },
-    { field: 'word_back', headerName: 'Back', width: 200, flex: 1 },
+    {
+        field: 'word_back',
+        headerName: 'Back',
+        width: 200,
+        flex: 1,
+        renderCell: (params) => (
+            <div
+                style={{
+                    width: '100%', // Use full width of the cell
+                    maxHeight: '100px', // Set maximum height for the cell content
+                    overflowY: 'auto', // Enable vertical scrolling
+                    whiteSpace: 'normal', // Allows text to wrap to the next line
+                    wordBreak: 'break-word', // Ensures words that are too long for the width will be broken and wrapped to the next line
+                }}
+            >
+                {params.value}
+            </div>
+        ),
+
+        },
     { field: 'deck', headerName: 'Deck', width: 150 },
 ];
 
-const rows = [
-    // Add your rows here. Example:
-    { id: 1, word_id: 1, word_front: 'Hello', word_back: '안녕하세요', deck: 'Basic Korean' },
-    { id: 1, word_id: 1, word_front: 'Hello', word_back: '안녕하세요', deck: 'Basic Korean' },
-    { id: 1, word_id: 1, word_front: 'Hello', word_back: '안녕하세요', deck: 'Basic Korean' },
-    { id: 1, word_id: 1, word_front: 'Hello', word_back: '안녕하세요', deck: 'Basic Korean' },
-];
+
 
 function DeckCardList() {
+    const [rows, setRows] = useState([]);
+    const { deck_name } = useParams(); // Access route parameter
+
+    useEffect(() => {
+        API.get(`${process.env.REACT_APP_API_URL}:3006/words_all/${deck_name}`) // Use deckName in the request
+            .then(response => {
+                console.log(response.data )
+                setRows(response.data.map((row, index) => ({ ...row, id: index }))); // Ensure rows have a unique 'id' prop
+                console.log(rows)
+            })
+            .catch(error => console.log(error));
+    }, [deck_name]); // Re-run effect if deckName changes
+
+
     return (
-        <div style={{ height: 400, width: '100%' }}>
+        <div style={{ height: '100%', width: '100%' }}>
             <DataGrid
                 rows={rows}
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
+                rowHeight={130} // Adjust the height as needed
+
             />
         </div>
     );
