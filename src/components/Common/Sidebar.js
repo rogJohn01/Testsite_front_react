8e089cffa52887061e6714d28167430d7b416789
존 <94162057+rogJohn01@users.noreply.github.com>
2024-02-15 15:40:09 +0900

    feat[sidebar]:  moved the signup and signin feature to the footer of siderbar

--- src/components/Common/Sidebar.js
+++ src/components/Common/Sidebar.js
@@ -29,6 +29,7 @@ import { DiCodeigniter } from "react-icons/di";
 
 import "./Sidebar.scss"
 import RecordDrill from "../../pages/RecordDrill";
+import { FaSignInAlt, FaUserPlus } from 'react-icons/fa'; // Importing icons
 
 
 const Sidebar = ({
@@ -38,6 +39,35 @@ const Sidebar = ({
   handleToggleSidebar,
   handleCollapsedChange
 }) => {
+
+
+  const isLoggedIn = !!localStorage.getItem('token');
+  console.log(isLoggedIn)
+  console.log(localStorage.getItem('token'))
+
+  const style = {
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      gap: '20px',
+    },
+    link: {
+      cursor: 'pointer',
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+    },
+    icon: {
+      marginRight: '8px',
+    },
+    span: {
+      fontSize: "15" ,
+      fontWeight: 'bold', // Make text bold
+      color: 'rgba(51, 0, 0, 1)', // Correct property for text color
+
+    }
+  };
+
   return (
     <ProSidebar
       collapsed={collapsed}
@@ -123,15 +153,28 @@ const Sidebar = ({
       </SidebarContent>
       {/* Footer */}
       <SidebarFooter style={{ textAlign: 'center' }}>
-        <div className="sidebar-btn-wrapper" style={{ padding: '16px' }}>
-          <Link
-            className="sidebar-btn"
-            style={{ cursor: 'pointer' }}
-            to="/profile"
-          >
-            <FaUser />
-            <span>My Account</span>
-          </Link>
+        <div className="sidebar-btn-wrapper" style={{ padding: '20px' }}>
+          {!isLoggedIn ? (
+              // Render these buttons if the user is not logged in
+              <>
+                <div style={style.container}>
+                  <Link to="/signin" className="sidebar-btn" style={style.link}>
+                    <FaSignInAlt style={style.icon} />
+                    <span style={style.span }>Sign In</span>
+                  </Link>
+                  <Link to="/signup" className="sidebar-btn" style={style.link}>
+                    <FaUserPlus style={style.icon} />
+                    <span style={style.span }>Sign up</span>
+                  </Link>
+                </div>
+              </>
+          ) : (
+              // Render this link if the user is logged in
+              <Link className="sidebar-btn" style={{ cursor: 'pointer' }} to="/profile">
+                <FaUser />
+                <span>My Account</span>
+              </Link>
+          )}
         </div>
       </SidebarFooter>
     </ProSidebar>
